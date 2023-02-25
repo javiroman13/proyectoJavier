1880c81dcc13c47dcd648c7b7049893049e75ecd
javier roman <22670018@itiguala.edu.mx>
2023-02-25 09:12:52 -0600

    se crea la clase proyectoJavier

--- proyectoJavier/src/Student.java
+++ proyectoJavier/src/Student.java
@@ -1,2 +1,69 @@
-package PACKAGE_NAME;public class Student {
-}
+public class Student {
+
+    //atributos
+        private String Name;
+        private String LastName;
+        private Integer ControlNumber;
+        private String career;
+        private String Semester;
+
+
+// metodos getter
+
+
+    public String getName() {
+        return this.Name;
+    }
+
+    public String getLastName() {
+        return this.LastName;
+    }
+
+    public long getControlNumber() {
+        return this.ControlNumber;
+    }
+
+    public String getCareer() {
+        return this.career;
+    }
+    public String getSemester() {
+        return this.Semester;
+    }
+
+    //metodos setter
+
+    public void setName(String name) {
+        this.Name=name;
+    }
+
+    public void setLastName(String apellido) {
+
+        this.LastName=apellido;
+    }
+
+    public void setControlNumber(Integer controlNumber) {
+        this.ControlNumber = controlNumber;
+    }
+
+    public void setSemester(String semester) {
+        this.Semester = semester;
+    }
+
+    public void setCareer(String career) {
+        this.career = career;
+    }
+
+
+    public void mostrarInf() {
+        System.out.println("el nombre es:"+getName()
+                +"\napellido:"+getLastName()
+                +"\nnumero de control:"+getControlNumber()
+                +"\ncarrera:"+getCareer()
+                +"\nsemetre:"+getSemester());
+
+
+    }
+    }
+
+
+
--- proyectoJavier/src/TestStudent.java
+++ proyectoJavier/src/TestStudent.java
@@ -1,2 +1,34 @@
-package PACKAGE_NAME;public class TestStudent {
+public class TestStudent {
+    public static void main(String[] args) {
+        System.out.println("probando clase student");
+        
+        Student javier = new Student();
+        javier.setName("javier");
+        javier.setLastName("roman");
+        javier.setCareer("ingenieria informatica");
+        javier.setControlNumber(22670018);
+        javier.setSemester("segundo");
+
+        Student estudend2 = new Student();
+        estudend2.setName("fernanda");
+        estudend2.setLastName("roman");
+        estudend2.setCareer("ingenieria imformatica");
+        estudend2.setSemester("segundo");
+        estudend2.setControlNumber(22670017);
+
+        estudend2.mostrarInf();
+
+        javier.mostrarInf();
+
+
+
+
+
+
+
+
+
+
+
+    }
 }
